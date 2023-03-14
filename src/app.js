a924b8ea1aea032c09d2c0d9d10ccc17469dc095
Vitor <vbrenochacon@hotmail.com>
2023-03-14 15:45:06 -0300

    feat: adicionada uma rota para a realização de login; users

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { loginRoute } = require('./routes');
 
 // ...
 
@@ -12,6 +13,8 @@ app.get('/', (_request, response) => {
 app.use(express.json());
 
 // ...
+
+app.use('/login', loginRoute);
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
