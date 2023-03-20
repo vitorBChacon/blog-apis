63bd3d80c66016b089bb41603b26202fe8a71647
Vitor <vbrenochacon@hotmail.com>
2023-03-20 10:15:15 -0300

    feat: adicionada rota para acessar lista de usuários; users

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -4,6 +4,7 @@ const { userController } = require('../controllers');
 const validateDisplayName = require('../middlewares/validateDisplayName');
 const validateEmail = require('../middlewares/validateEmail');
 const validatePassword = require('../middlewares/validatePassword');
+const validateToken = require('../middlewares/validateToken');
 const validateUser = require('../middlewares/validateUser');
 
 const router = express.Router();
@@ -14,5 +15,7 @@ router.post('/',
   validatePassword,
   validateUser,
   userController.createUser);
+
+router.get('/', validateToken, userController.getUsers);
 
 module.exports = router;
