d18e07a82c9645fa8c07a095a704609e9ba3a1a1
Vitor <vbrenochacon@hotmail.com>
2023-03-20 09:58:24 -0300

    feat: middleware de validação de token adicionado

---
+++ src/middlewares/validateToken.js
@@ -0,0 +1,20 @@
+const { verifyToken } = require('../auth/authFunctions');
+
+const validateToken = (req, res, next) => {
+  try {
+    const { authorization } = req.headers;
+    if (!authorization) {
+      return res.status(401).json({ message: 'Token not found' });
+    }
+    const payload = verifyToken(authorization);
+    req.data = payload.data;
+    next();
+  } catch (error) {
+    res.status(500).json({
+      message: 'Erro ao acessar o endpoint',
+      error: error.message,
+    });
+  }
+};
+
+module.exports = validateToken;
